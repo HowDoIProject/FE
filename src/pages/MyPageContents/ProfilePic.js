2a6cb780fd7f0df3b8800218d392e71d71508ca5
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-06-24 18:19:08 +0900

    파싱 에러 고침

--- src/pages/MyPageContents/ProfilePic.js
+++ src/pages/MyPageContents/ProfilePic.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from 'react';
 import { FaUserCircle, FaEdit } from 'react-icons/fa';
-import { doggy } from '../../assets/doggy.svg';
 
 const ProfilePicture = () => {
     const [selectedFile, setSelectedFile] = useState(null);
@@ -32,7 +31,7 @@ const ProfilePicture = () => {
                 ref={fileInputRef}
             />
             {selectedFile ? (
-                <img className="absolute top-[186px] left-[30px] w-20 h-20" alt="" src="/group-7387.svg" />
+                <img src={URL.createObjectURL(selectedFile)} alt="Profile" className="w-full h-full" />
             ) : (
                 <FaUserCircle className="w-full h-full text-gray-800" onClick={handleEditClick} />
             )}
