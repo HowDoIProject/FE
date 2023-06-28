240519c7ddad5cdda86183707f3bcf962b9a7905
Davin <davinrim10@gmail.com>
2023-06-28 17:39:32 +0900

    메인페이지 가로스크롤 tailwind로 구현해서 수정

--- src/components/MainPageCard.js
+++ src/components/MainPageCard.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function MainPageCard({ post }) {
     return (
-        <div className="w-full h-full bg-[#F3F3F3]">
-            <h1 className="">{post.title}</h1>
+        <div className="w-full h-full flex flex-col gap-6 px-4 pt-4 pb-10 items-center bg-[#F3F3F3]">
+            <div>{post.category}</div>
+            <h1 className="line-clamp-2">{post.title}</h1>
         </div>
     );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,32 +1,58 @@
 import React from 'react';
 import MainPageCard from '../components/MainPageCard';
-import { motion } from 'framer-motion';
 import { useRef, useState, useEffect } from 'react';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { apiPosts } from '../shared/Api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 export default function Main() {
-    const topFiveRef = useRef(null);
     const queryClient = useQueryClient();
     const { data: topFive, error, isLoading } = useQuery(['posts', 'topfive'], () => apiPosts.getTopFive());
+
     console.log(topFive?.data.topfive);
+
+    const slideLeft = () => {
+        const slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft - 280;
+    };
+
+    const slideRight = () => {
+        const slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft + 280;
+    };
 
     return (
         <>
             <div>안녕하세요 카드</div>
             <section>
                 <span>top5 글</span>
-                <motion.div ref={topFiveRef} className="overflow-hidden">
-                    <motion.div drag="x" dragConstraints={topFiveRef} className="flex">
+                <div className="relative flex items-center">
+                    <MdChevronLeft
+                        className="opacity-50 cursor-pointer hover:opacity-100"
+                        onClick={slideLeft}
+                        size={30}
+                    />
+                    <div
+                        id="slider"
+                        className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+                    >
                         {topFive?.data.topfive.map(post => {
                             return (
-                                <motion.div key={post.post_id} className="min-w-[144px] min-h-[144px] pr-2">
+                                <div
+                                    key={post.post_id}
+                                    className="w-[146px] h-[146px] pr-2 inline-block cursor-pointer hover:scale-105 ease-in-out duration-300"
+                                >
                                     <MainPageCard post={post} />
-                                </motion.div>
+                                </div>
                             );
                         })}
-                    </motion.div>
-                </motion.div>
+                    </div>
+                    <MdChevronRight
+                        className="opacity-50 cursor-pointer hover:opacity-100"
+                        onClick={slideRight}
+                        size={30}
+                    />
+                </div>
             </section>
             <div>
                 <span>실시간 글</span>
