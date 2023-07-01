4819aee19f0332e322a4ee18f947a16efb63c2e5
Davin <davinrim10@gmail.com>
2023-07-01 09:46:07 +0900

    게시판페이지 무한스크롤

--- src/components/WelcomeCard.js
+++ src/components/WelcomeCard.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import image from '../assets/icon/dog_1.svg';
+import image from '../assets/icon/welcome.svg';
 
 export default function WelcomeCard() {
     return (
-        <div className="w-full h-[146px] inline-flex px-6 py-4 justify-center items-center bg-[#F3F3F3]">
+        <div className="w-full h-[146px] inline-flex px-6 py-4 justify-center items-center rounded-xl bg-bgcard shadow-mainbox">
             <div className="flex flex-col gap-4 items-start">
-                <h1 className="text-bold">자취고민을 해결해줄 하우두아이!</h1>
-                <h1 className="line-clamp-2">로그인 후 정보 확인이 가능합니다</h1>
+                <h1 className="font-bold">자취고민을 함께 나누고 해결하는 하우두아이!</h1>
+                <h3 className="text-gray_02 font-light text-xl">로그인 후 정보 확인이 가능합니다</h3>
             </div>
-            <div className="w-20 h-20 flex justify-center items-center rounded-full bg-[#D9D9D9]">
-                <img src={image} alt="" />
-            </div>
+            <img src={image} alt="" />
         </div>
     );
 }
