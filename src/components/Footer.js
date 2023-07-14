1d0208759945bc1f56fab02f30afdd9c549b6dff
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-07-14 21:59:12 +0900

    회원별 footer 적용

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,40 @@ import feed from '../assets/icon/feed.svg';
 import mypage from '../assets/icon/mypage.svg';
 import scrap from '../assets/icon/scrap.svg';
 
-export default function Footer() {
+export default function Footer({ user_type }) {
+    if (user_type !== '강아지') {
+        return (
+            <div className="fixed bottom-0 w-full h-[80px] bg-white border-t-[0.5px] border-slate-300">
+                <div className="w-full h-[56px] max-w-[420px] mt-2 mx-auto bg-white flex justify-evenly items-center">
+                    <a href="/" className="no-underline hover:text-decoration-underline">
+                        <div className="flex flex-col min-w-[94px] items-center justify-center gap-1">
+                            <img src={home} alt={home} />
+                            <h1>메인</h1>
+                        </div>
+                    </a>
+                    <a href="/posts" className="no-underline hover:text-decoration-underline">
+                        <div className="flex flex-col min-w-[94px] items-center justify-center gap-1">
+                            <img src={feed} alt={feed} />
+                            <h1>게시판</h1>
+                        </div>
+                    </a>
+                    <a href="/write" className="no-underline hover:text-decoration-underline">
+                        <div className="flex flex-col min-w-[94px] items-center justify-center gap-1">
+                            <img src={post} alt={post} />
+                            <h1>글 올리기</h1>
+                        </div>
+                    </a>
+                    <a href="/mypage" className="no-underline hover:text-decoration-underline">
+                        <div className="flex flex-col min-w-[94px] items-center justify-center gap-1">
+                            <img src={mypage} alt={mypage} />
+                            <h1>마이페이지</h1>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="fixed bottom-0 w-full h-[80px] bg-white border-t-[0.5px] border-slate-300">
             <div className="w-full h-[56px] max-w-[420px] mt-2 mx-auto bg-white flex justify-evenly items-center">
