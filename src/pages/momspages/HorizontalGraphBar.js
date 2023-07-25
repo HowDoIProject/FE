036351b9f6af50bd9f1f91cdf5a411d05b894888
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-07-25 20:08:03 +0900

    conflict 수정

--- src/pages/momspages/HorizontalGraphBar.js
+++ src/pages/momspages/HorizontalGraphBar.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProgressBar from '@ramonak/react-progress-bar';
 import { useCookies } from 'react-cookie';
+import parents from '../../assets/icon/welcomeparents.svg';
 
-const HorizontalBarGraph = () => {
+const HorizontalBarGraph = ({ nickname }) => {
     const [graphData, setGraphData] = useState({
-        mypost: 0,
-        mycomment: 0,
-        mylikes: 0,
+        mypost: 1,
+        mycomment: 1,
+        mylikes: 1,
     });
     const [cookies] = useCookies(['accessToken']);
 
@@ -29,38 +30,49 @@ const HorizontalBarGraph = () => {
         fetchData();
     }, [cookies]);
 
-    const progressData = [
-        {
-            label: `내 작성글 ${graphData.mypost}개`,
-            bgColor: '#4CAF50',
-            completed: graphData.mypost,
-        },
-        {
-            label: `댓글 ${graphData.mycomment}개`,
-            bgColor: '#FFC107',
-            completed: graphData.mycomment,
-        },
-        {
-            label: `좋아요 ${graphData.mylikes}개`,
-            bgColor: '#2196F3',
-            completed: graphData.mylikes,
-        },
-    ];
+    console.log('graphData', graphData);
 
     return (
         <div>
-            {progressData.map((data, index) => (
-                <div key={data.label} className={index !== 0 ? 'mt-4' : ''}>
-                    <div className="d-flex align-items-center">
-                        <ProgressBar completed={data.completed} label={data.label} bgColor={data.bgColor} />
-                        <span className="ml-2">
-                            {index === 0 && '나의 작성글'}
-                            {index === 1 && '내 댓글'}
-                            {index === 2 && '좋아요'}
-                        </span>
+            <div className="w-full h-[216px] px-6 py-4 justify-between items-center rounded-xl bg-bgcard shadow-mainbox">
+                <div className="flex justify-between border-b border-gray_03 pb-4 items-center">
+                    <div className="flex flex-col items-start gap-1">
+                        <h1 className="font-['Pretendard-Bold']">안녕하세요!</h1>
+                        <h1 className="font-['Pretendard-Bold'] w-[180px] line-clamp-1">{nickname}님</h1>
                     </div>
+                    <img src={parents} alt="" />
                 </div>
-            ))}
+                <div className="flex items-center justify-between mt-4">
+                    <div className="text-[14px]">게시글</div>
+                    <ProgressBar
+                        completed={graphData.mypost * 10}
+                        bgColor="#FFA41B"
+                        width="240px"
+                        height="10px"
+                        labelClassName="label"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <div className="text-[14px]">답변</div>
+                    <ProgressBar
+                        completed={graphData.mycomment * 10}
+                        bgColor="#FFA41B"
+                        width="240px"
+                        height="10px"
+                        labelClassName="label"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <div className="text-[14px]">도움됐어요</div>
+                    <ProgressBar
+                        completed={graphData.mylikes * 10}
+                        bgColor="#FFA41B"
+                        width="240px"
+                        height="10px"
+                        labelClassName="label"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
